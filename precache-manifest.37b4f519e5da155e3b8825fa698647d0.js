6305df84951efc918da4b50dcf589e5bb2d8477d
Julio  Vásconez <jvasconez28@gmail.com>
2019-09-01 22:34:42 +0200

    Updates

---
+++ precache-manifest.37b4f519e5da155e3b8825fa698647d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad42663cfd4ceec204d9b1998f10d4f8",
+    "url": "/react-socketio-chat-app/index.html"
+  },
+  {
+    "revision": "29508db8d26cf597b0c5",
+    "url": "/react-socketio-chat-app/static/css/main.7a33ba05.chunk.css"
+  },
+  {
+    "revision": "340c038249fb9cf035d2",
+    "url": "/react-socketio-chat-app/static/js/2.c2d3640a.chunk.js"
+  },
+  {
+    "revision": "29508db8d26cf597b0c5",
+    "url": "/react-socketio-chat-app/static/js/main.9ba5d81b.chunk.js"
+  },
+  {
+    "revision": "362addcd19c122023cdf",
+    "url": "/react-socketio-chat-app/static/js/runtime~main.44e56ef7.js"
+  }
+]);
